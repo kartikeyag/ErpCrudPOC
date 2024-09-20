4aed1339c22e0a44dc062c624e9c8ec8166ac10e
riya gupta <riyagupta@riyas-MacBook-Air.local>
2024-09-20 16:57:45 +0530

    Spring AI integrated

--- src/main/java/org/springframework/samples/erpcrud/ErpCrudApplication.java
+++ src/main/java/org/springframework/samples/erpcrud/ErpCrudApplication.java
@@ -16,14 +16,34 @@
 
 package org.springframework.samples.erpcrud;
 
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.ollama.OllamaEmbeddingModel;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
+import org.springframework.ai.vectorstore.PgVectorStore;
+import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportRuntimeHints;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.core.DataClassRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.JdbcClient;
+
+import java.util.List;
+import java.util.Map;
 
 /**
- * PetClinic Spring Boot Application.
+ * PetClinic Spring Boot Application. -Dspring.profiles.active=postgres
  *
  * @author Dave Syer requestMatchers("/").hasAnyRole("USER")
+ * https://github.com/spring-tips/vector-databases/tree/main
+ * #https://spring.io/guides/gs/securing-web
+ * https://medium.com/@vishal.sharma./run-postgresql-and-pgadmin-using-docker-compose-34120618bcf9
  */
 @SpringBootApplication
 @ImportRuntimeHints(PetClinicRuntimeHints.class)
@@ -33,4 +53,53 @@ public class ErpCrudApplication {
 		SpringApplication.run(ErpCrudApplication.class, args);
 	}
 
+	@Bean
+	@Profile("postgres")
+	TokenTextSplitter tokenTextSplitter() {
+		return new TokenTextSplitter();
+	}
+
+	private final boolean ingest = true;
+
+	@Bean
+	@Profile("postgres")
+	ApplicationRunner demo(TokenTextSplitter tokenTextSplitter, JdbcClient db, VectorStore vectorStore) {
+		return args -> {
+
+			var products = db.sql("select * from owners").query(new DataClassRowMapper<>(Product.class)).list();
+
+			if (this.ingest) {
+
+				products.parallelStream().forEach(product -> {
+					var document = new Document(product.first_name() + " " + product.last_name(),
+							Map.of("phone", product.telephone(), "id", product.id(), "firstname", product.first_name(),
+									"lastname", product.last_name(), "address", product.address()));
+
+					var split = tokenTextSplitter.apply(List.of(document));
+					vectorStore.add(split);
+				});
+
+			}
+
+			// var single = products
+			// .parallelStream()
+			// .filter(product -> product.id() == 115)
+			// .toList()
+			// .getFirst();
+
+			var similar = vectorStore.similaritySearch(SearchRequest.defaults().query("Fair Way").withTopK(10));
+			System.out.println("count: " + similar.size());
+			for (var s : similar) {
+				System.out.println("===========");
+				var id = (s.getMetadata().get("id"));
+				System.out.println("id: " + id);
+				s.getMetadata().forEach((k, v) -> System.out.println(k + '=' + v));
+			}
+
+		};
+	}
+
+}
+
+record Product(int id, String first_name, String last_name, String address, String telephone) {
 }
